ff028d3d44ac49fde8da7f90b8975312d6692610
Tom Fenech <tomjwfenech@gmail.com>
2019-09-15 19:36:44 +0200

    Allow hook ID to be set via HOOK env var

--- main.go
+++ main.go
@@ -2,34 +2,41 @@ package main
 
 import (
 	"context"
-	"io/ioutil"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+var client = &http.Client{
+	Timeout: 10 * time.Second,
+}
+
 type Event struct {
 	Hook string `json:"hook"`
 }
 
-func HandleRequest(ctx context.Context, event Event) ([]byte, error) {
-	client := &http.Client{
-		Timeout: 10 * time.Second,
+func HandleRequest(ctx context.Context, event Event) error {
+	var hook string
+	if event.Hook == "" {
+		hook = os.Getenv("HOOK")
+	} else {
+		hook = event.Hook
 	}
 
-	req, err := http.NewRequest(http.MethodPost, "https://api.netlify.com/build_hooks/"+event.Hook, nil)
+	req, err := http.NewRequest(http.MethodPost, "https://api.netlify.com/build_hooks/"+hook, nil)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	res, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer res.Body.Close()
 
-	return ioutil.ReadAll(res.Body)
+	return nil
 }
 
 func main() {
